35a621380908cc0c0eae74e7c177b28e4668d6db
MC3D <m.chapman.home@gmail.com>
2014-09-22 13:55:43 -0400

    initial commit

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
+//run this function on page load; $(document).ready does not wait for all images to load before firing
 $(document).ready(function() {
-	$("#menu section p").on('click',function(e) {
+  $(".level-one").on('click', function() {
 
-		if ( $(this).parents("section").hasClass("hide") ){
+    $(".level-two").addClass("hide");
 
-		$(this).parents().siblings("section").addClass("hide");
-		$(this).parents("section").removeClass("hide");}
+    if ($(this).parents("section").hasClass("hide")) {
+      $(this).parents().siblings("section").addClass("hide");
+      $(this).parents("section").removeClass("hide");
+    } else {
+      $(this).parents("section").addClass("hide");
+    }
+  });
 
-		else{
-			$(this).parents("section").addClass("hide");
-		}
-	});
+  $(".level-two").on('click', function(e) {
+    if ($(this).hasClass("hide")) {
+      $(this).removeClass("hide");
+      $(this).siblings(".level-two").addClass("hide");
+    } else {
+      $(this).addClass("hide");
+    }
+  });
 })(jQuery);
-
-// $(document).ready == page load; does not wait for all the images to load before firing
-//function() == function without parameters
-//'click'; run this function when the menu item is clicked
-//function(e); e is the event object that is populated when the event callback is executed
-//(this) refers to the object on which a method is being invoked
-//In this case, "this" references the menu surface items
-	//add class hide to parent siblings
-	//remove class from parent (the menu item you clicked)
-//WHAT IS e.preventDefault() USED FOR??? IS return false; BETTER???
